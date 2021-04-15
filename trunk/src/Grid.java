a1fe5185a220a71f19a847398abee183165159de
yun-yan-cheng-yu <1090042044@qq.com>
2021-04-16 00:33:44 +0800

    完成摒弃和决定数字

--- trunk/src/Grid.java
+++ trunk/src/Grid.java
@@ -1,19 +1,105 @@
 import java.awt.*;
 
-public class Grid extends Button {
+public class Grid extends Panel {
+
     int num = 0;
+
+    int [][] nums = {{1,2,3},{4,5,6},{7,8,9}};//当num确定下来后，该数据失效
+
+
+    Grid(){
+        this.setLayout(new GridLayout(3,3,5,5));
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                Button b = new Button();
+                b.setLabel(nums[i][j]+"");
+                this.add(b);
+            }
+        }
+    }
 
     /**
      * 设置一个格子的数字，一旦设置成功，格子将无法操作
      * @param num 要设置的数字，位于1-9之间
      */
-    void setNum(int num){
+    void setAnswer(int num){
         if(num <= 0 || num > 9){
             System.err.println("Grid 设置数字异常:"+num);
             return;
         }
+        this.removeAll();
+        this.setLayout(new GridLayout(1,1,5,5));
         this.num = num;
-        this.setLabel(num+"");
-        this.setEnabled(false);
+        Button b = new Button();
+        b.setLabel(num+"");
+        b.setEnabled(false);
+
+        this.add(b);
+    }
+
+    /**
+     * 判断该格子数字是否确定下来
+     * @return 确定则true
+     */
+    boolean isDecided(){
+        return this.num > 0 && this.num < 10;
+    }
+
+    /**
+     * 根据当前数字渲染当前的button
+     */
+    void render(){
+        if(isDecided()){
+            return ;
+        }
+        this.removeAll();
+        this.setLayout(new GridLayout(3,3,5,5));
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                Button b = new Button();
+                b.setLabel(nums[i][j]==0?"":nums[i][j]+"");
+                this.add(b);
+            }
+        }
+    }
+
+    int canDecide(){
+        int count = 0;
+        int res = 0;
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(nums[i][j] != 0){
+                    count++;
+                    res = nums[i][j];
+                }
+            }
+        }
+        if(count==1){
+            return res;
+        }
+        return 0;
+    }
+
+    /**
+     * 决定该出数字
+     */
+    void decideNum(){
+        if(isDecided()){
+            return ;
+        }
+        if(canDecide() == 0){
+            return ;
+        }
+
+        num = canDecide();
+
+        this.removeAll();
+        this.setLayout(new GridLayout(1,1,5,5));
+
+        Button b = new Button();
+        b.setLabel(num+"");
+        b.setEnabled(false);
+
+        this.add(b);
     }
 }
--- trunk/src/ShuDu.java
+++ trunk/src/ShuDu.java
@@ -1,25 +1,103 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ShuDu {
     Frame frame = new Frame();//一个框架
     Grid[][] grids = new Grid[9][9];//9*9的格子
 
     ShuDu(int[][] array){
-        frame.setLayout(new GridLayout(9,9));
-        frame.setVisible(true);
+        frame.setLayout(new GridLayout(9,9,10,10));
         frame.setLocation(600,300);
 
         for (int i = 0;i < 9; i++){
             for(int j = 0; j < 9;j++){
                 Grid g = new Grid();
-                g.setNum(array[i][j]);
+                g.setAnswer(array[i][j]);
 
                 grids[i][j] = g;
                 frame.add(grids[i][j]);
             }
         }
+        MenuBar m = new MenuBar();
+        Menu operation = new Menu("operation");
+        MenuItem throwNumThatImpossible = new MenuItem("throwNumThatImpossible");
+        MenuItem decideNum = new MenuItem("decideNum");
+
+        frame.setMenuBar(m);
+        m.add(operation);
+        operation.add(throwNumThatImpossible);
+        operation.add(decideNum);
+
         frame.pack();
+        frame.setBackground(Color.PINK);
+
+        frame.setVisible(true);
+        //事件
+        throwNumThatImpossible.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("throwNumThatImpossible");
+                throwNumThatImpossible();
+                renderAll();
+            }
+        });
+
+        decideNum.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                decideNum();
+            }
+        });
     }
 
+    /**
+     * 去除那些不可能的数字
+     * 根据其他相关格去除掉每个格子的待选数字
+     */
+    public void throwNumThatImpossible(){
+        for (int i = 0;i < 9; i++){
+            for(int j = 0; j < 9;j++){
+                if(grids[i][j].isDecided()){//只有该格子数字确定下来才能去除其他格子数据
+                    int num = grids[i][j].num;
+                    for(int temp = 0; temp < 9; temp++){
+                        grids[i][temp].nums[(num-1)/3][(num-1)%3] = 0;//去除一行
+                        grids[temp][j].nums[(num-1)/3][(num-1)%3] = 0;//去除一列
+                    }
+                    //去除宫比较复杂
+                    int start_i = (i/3) * 3;
+                    int start_j = (j/3) * 3;
+                    for (int m = 0; m < 3; m++){
+                        for(int n = 0; n < 3;n++){
+                            grids[start_i + m][start_j+n].nums[(num-1)/3][(num-1)%3] = 0;//去除一宫
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 根据数字渲染图形
+     */
+    public void renderAll(){
+        frame.setVisible(false);
+        for (int i = 0; i < 9; i++){
+            for(int j = 0; j < 9;j++){
+                grids[i][j].render();
+            }
+        }
+        frame.setVisible(true);
+    }
+
+    public void decideNum(){
+        frame.setVisible(false);
+        for (int i = 0; i < 9; i++){
+            for(int j = 0; j < 9;j++){
+                grids[i][j].decideNum();
+            }
+        }
+        frame.setVisible(true);
+    }
 
 }
